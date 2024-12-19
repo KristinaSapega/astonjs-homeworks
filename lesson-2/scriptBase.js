30af557f7cf5be485b6a9206994f2fa8b8894423
Kristina <tinasapega@gmail.com>
2024-12-19 16:42:54 +0300

    ASTONJS-2 [chore]: base. added description of what was done

--- lesson-2/scriptBase.js
+++ lesson-2/scriptBase.js
@@ -1,3 +1,6 @@
+//Написать функцию getLength, принимающую на вход валидное JS значение любого типа. 
+//Функция должна вывести в консоль длину (length) этого значения, если это возможно, иначе вывести в консоль 0.
+
 function getLength(value) {
     if (value !== null && value !== undefined && typeof value.length === 'number') {
         console.log(value.length);
